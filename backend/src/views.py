2bcf065bf5e7507ca378d69cd56b3025077ff2fc
bajaco <brett@agyx.org>
2020-10-11 07:42:34 +0000

    fix: error in views.py

--- backend/src/views.py
+++ backend/src/views.py
@@ -4,7 +4,6 @@ from src.database.models import User
 @app.route('/', methods=['GET'])
 def hello():
     testing = User(username='Test_User', email='test@email')
-    testing
     
     return ' '.join([str(user.id) + ' ' + user.username for user in User.query.all()])
 
