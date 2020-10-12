6259f60464021e4cfa2b5c6c08b9a259c0e32336
bajaco <brett@agyx.org>
2020-10-12 08:37:51 +0000

    Feat: Basic Model implementation
    Implementation of four models with test in view
    to verify working relationships

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,13 +1,65 @@
 from . import db
 
+# User class with linking id to auth0id
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=False, nullable=False)
-    email = db.Column(db.String(120), unique=False, nullable=False)
+    auth0_id = db.Column(db.String(600), nullable=False)
+    unpublished = db.relationship('Unpublished_Tutorial')
+    #published = db.relationship('Published_Tutorial')
+
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % str(self.id)
 
     def insert(self):
         db.session.add(self)
         db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+# Association table between unpublished tutorials and tags
+tutorial_tag = db.Table('tutorials_tags', db.Model.metadata,
+        db.Column('left_id', db.Integer, db.ForeignKey('unpublished_tutorials.id')),
+        db.Column('right_id', db.Integer, db.ForeignKey('tags.id'))
+        )
+# Unpublished tutorials, separately implemented from published
+# tutorials so changes can be acccepted before publishing without
+# causing the published tutorial to cease to be displayed
+class Unpublished_Tutorial(db.Model):
+    __tablename__ = 'unpublished_tutorials'
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    title = db.Column(db.String(), nullable=False)
+    text = db.Column(db.String(), nullable=False)
+    tags = db.relationship('Tag',
+        secondary=tutorial_tag,
+        back_populates='unpublished_tutorials')
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+# Tags to be applied to tutorials when submitted
+class Tag(db.Model):
+    __tablename__ = 'tags'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    unpublished_tutorials = db.relationship(
+        'Unpublished_Tutorial',
+        secondary=tutorial_tag,
+        back_populates='tags')
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+
+
--- backend/src/views.py
+++ backend/src/views.py
@@ -1,9 +1,33 @@
+from flask import jsonify
 from . import app
 from .models import User
+from .models import Tag
+from .models import Unpublished_Tutorial
+
 
 @app.route('/', methods=['GET'])
 def hello():
-    testing = User(username='Test_User', email='test@email')
-    testing.insert() 
-    return ' '.join([str(user.id) + ' ' + user.username for user in User.query.all()])
+    user = User(auth0_id='FAKEAUTH0ID')
+    user.insert()
+    tag1 = Tag(name='Flask')
+    tag1.insert()
+    tag2 = Tag(name='Postgres')
+    tag2.insert()
+    tag3 = Tag(name='Docker')
+    tag3.insert()
+    tutorial = Unpublished_Tutorial(
+            author_id=user.id,
+            title='Great Tutorial with flask',
+            text='The content of a great tutorial'
+            )
+    tutorial.tags.append(tag1)
+    tutorial.tags.append(tag2)
+    tutorial.tags.append(tag3)
+    tutorial.insert()
+
+    tutorial = user.unpublished
+    return str(user.unpublished[0].author_id) + ' ' + ' '.join([str(tag.name) for tag in user.unpublished[0].tags])
+
+
+   
 
