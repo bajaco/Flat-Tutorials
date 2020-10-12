74bbf0fecdb52fa60b7e70711667ee74cfdb55e4
bajaco <brett@agyx.org>
2020-10-12 11:39:08 +0000

    Feat: Working endpoints
    Working endpoints for unpublished tutorial(s)
    published tutorial(s)
    publishing a tutorial

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,16 +1,35 @@
 from . import db
 
+# Helper functions
+
+def dictit(obj, *args):
+    new_dict = {}
+    for arg in args:
+        result = getattr(obj, arg)
+        if isinstance(result,list):
+            result = [str(i) for i in result]
+        else:
+            result = str(result)
+        new_dict[arg] = result
+        
+    return new_dict
+
+
 # User class with linking id to auth0id
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     auth0_id = db.Column(db.String(600), nullable=False)
-    unpublished = db.relationship('Unpublished_Tutorial')
-    #published = db.relationship('Published_Tutorial')
-
+    username = db.Column(db.String(20))
+    unpublished = db.relationship('Unpublished_Tutorial',
+            backref='author',
+            lazy=True)
+    published = db.relationship('Published_Tutorial',
+            backref='author',
+            lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % str(self.id)
+        return self.username or 'unknown user'
 
     def insert(self):
         db.session.add(self)
@@ -19,11 +38,22 @@ class User(db.Model):
     def update(self):
         db.session.commit()
 
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
 # Association table between unpublished tutorials and tags
-tutorial_tag = db.Table('tutorials_tags', db.Model.metadata,
+unpublished_tutorial_tag = db.Table('unpublished_tutorials_tags', db.Model.metadata,
         db.Column('left_id', db.Integer, db.ForeignKey('unpublished_tutorials.id')),
         db.Column('right_id', db.Integer, db.ForeignKey('tags.id'))
         )
+
+# Association table between published tutorials and tags
+published_tutorial_tag = db.Table('published_tutorials_tags', db.Model.metadata,
+        db.Column('left_id', db.Integer, db.ForeignKey('published_tutorials.id')),
+        db.Column('right_id', db.Integer, db.ForeignKey('tags.id'))
+        )
+
 # Unpublished tutorials, separately implemented from published
 # tutorials so changes can be acccepted before publishing without
 # causing the published tutorial to cease to be displayed
@@ -34,8 +64,36 @@ class Unpublished_Tutorial(db.Model):
     title = db.Column(db.String(), nullable=False)
     text = db.Column(db.String(), nullable=False)
     tags = db.relationship('Tag',
-        secondary=tutorial_tag,
+        secondary=unpublished_tutorial_tag,
         back_populates='unpublished_tutorials')
+    
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+ 
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()   
+
+    def short(self):
+        return dictit(self,'id','author','title','tags')
+       
+    def long(self):
+        return dictit(self,'id','author','title','tags','text')
+     
+# Published tutorials
+class Published_Tutorial(db.Model):
+    __tablename__ = 'published_tutorials'
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    title = db.Column(db.String(), nullable=False)
+    text = db.Column(db.String(), nullable=False)
+    tags = db.relationship('Tag',
+        secondary=published_tutorial_tag,
+        back_populates='published_tutorials')
 
     def insert(self):
         db.session.add(self)
@@ -44,6 +102,16 @@ class Unpublished_Tutorial(db.Model):
     def update(self):
         db.session.commit()
 
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    def short(self):
+        return dictit(self,'id','author','title','tags')
+       
+    def long(self):
+        return dictit(self,'id','author','title','tags','text')
+
 # Tags to be applied to tutorials when submitted
 class Tag(db.Model):
     __tablename__ = 'tags'
@@ -51,8 +119,16 @@ class Tag(db.Model):
     name = db.Column(db.String(50), unique=True)
     unpublished_tutorials = db.relationship(
         'Unpublished_Tutorial',
-        secondary=tutorial_tag,
+        secondary=unpublished_tutorial_tag,
         back_populates='tags')
+    
+    published_tutorials = db.relationship(
+        'Published_Tutorial',
+        secondary=published_tutorial_tag,
+        back_populates='tags')
+
+    def __repr__(self):
+        return self.name
 
     def insert(self):
         db.session.add(self)
--- backend/src/views.py
+++ backend/src/views.py
@@ -1,33 +1,79 @@
 from flask import jsonify
+from flask import abort
 from . import app
-from .models import User
-from .models import Tag
-from .models import Unpublished_Tutorial
+from .models import *
+
+# get all short form unpublished tutorials
+@app.route('/unpublished', methods=['GET'])
+def get_unpublished_list():
+    query = Unpublished_Tutorial.query.all()
+    result = [tut.short() for tut in query]
+    return jsonify({
+        'success': True,
+        'tutorials': result
+        }), 200
+    
+#get long form of unpublished tutorial
+@app.route('/unpublished/<int:tutorial_id>', methods=['GET'])
+def get_unpublished_tutorial(tutorial_id):
+    result = Unpublished_Tutorial.query.get(tutorial_id).long()
+    return jsonify({
+        'success': True,
+        'tutorial': result
+    }), 200
+
+#get all short form published tutorials
+@app.route('/published', methods=['GET'])
+def get_published_list():
+    query = Published_Tutorial.query.all()
+    result = [tut.short() for tut in query]
+    return jsonify({
+        'success': True,
+        'tutorials': result
+        }), 200
+
+#get long form of published tutorial
+@app.route('/published/<int:tutorial_id>', methods=['GET'])
+def get_published_tutorial(tutorial_id):
+    result = Published_Tutorial.query.get(tutorial_id).long()
+    return jsonify({
+        'success': True,
+        'tutorial': result
+    }), 200
 
 
-@app.route('/', methods=['GET'])
-def hello():
-    user = User(auth0_id='FAKEAUTH0ID')
-    user.insert()
-    tag1 = Tag(name='Flask')
-    tag1.insert()
-    tag2 = Tag(name='Postgres')
-    tag2.insert()
-    tag3 = Tag(name='Docker')
-    tag3.insert()
-    tutorial = Unpublished_Tutorial(
-            author_id=user.id,
-            title='Great Tutorial with flask',
-            text='The content of a great tutorial'
-            )
-    tutorial.tags.append(tag1)
-    tutorial.tags.append(tag2)
-    tutorial.tags.append(tag3)
-    tutorial.insert()
+#create or update published tutorial by copying unpublished
+#returns newly published tutorial
+@app.route('/publish/<int:tutorial_id>', methods=['GET'])
+def publish(tutorial_id):
+    unpublished = Unpublished_Tutorial.query.get(tutorial_id)
+    published = Published_Tutorial.query.get(tutorial_id)
+    
+    #if it doesn't exist create it
+    update = False
+    if published:
+        update = True
+    else:
+        published = Published_Tutorial(id=unpublished.id)
+    
+    #make updates
+    published.author_id = unpublished.author_id
+    published.title = unpublished.title
+    published.text = unpublished.text
+    published.tags = unpublished.tags
+    
+    #update or insert depending on whether it exists
+    if update:
+        published.update()
+    else:
+        published.insert()
+    
+    #return newly published tutorial
+    result = Published_Tutorial.query.get(tutorial_id).long()
+    return jsonify({
+        'success': True,
+        'updated': update,
+        'tutorial': result
+        }), 200
 
-    tutorial = user.unpublished
-    return str(user.unpublished[0].author_id) + ' ' + ' '.join([str(tag.name) for tag in user.unpublished[0].tags])
-
-
-   
 
