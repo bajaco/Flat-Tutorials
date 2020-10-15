40fac7f7de6bb2f45d116182a8a708ee1108ef13
bajaco <admin@agyx.org>
2020-10-15 23:48:35 +0000

    Fix: Edited down tests.
    Removed tests that were excessively extraneous.

--- backend/src/test_app.py
+++ backend/src/test_app.py
@@ -2,58 +2,6 @@
 # admin tests #
 ###############
 
-def test_admin_get_published_list(self):
-    res = self.client().get('/published')
-    pass
-
-def test_admin_get_published_tutorial(self):
-    res = self.client().get('/published/<int:tutorial_id>')
-    pass
-
-def test_admin_get_published_list_by_author(self):
-    res = self.client().get('/published/by-author/<int:author_id>')
-    pass
-
-def test_admin_get_published_by_tag(self):
-    res = self.client().get('/published/tags/<string:tag>')
-    pass
-
-def test_admin_get_published_by_tags(self):
-    res = self.client().get('/published/tags/<string:tag1>/<string:tag2>')
-    pass
-
-def test_admin_submit(self):
-    res = self.client().post('/submit')
-    pass
-
-def test_admin_edit(self):
-    res = self.client().patch('/edit/<int:tutorial_id>')
-    pass
-
-def test_admin_get_submitted_list(self):
-    res = self.client().get('/submitted')
-    pass
-
-def test_admin_get_submitted_tutorial(self):
-    res = self.client().get('/submitted<int:tutorial_id>')
-    pass
-
-def test_admin_get_unpublished_list(self):
-    res = self.client().get('/unpublished')
-    pass
-
-def test_admin_get_unpublished_tutorial(self):
-    res = self.client().get('/unpublished/<int:tutorial_id>')
-    pass
-
-def test_admin_publish(self):
-    res = self.client().get('/publish/<int:tutorial_id>')
-    pass
-
-def test_admin_deny(self):
-    res = self.client().patch('/deny/<int:tutorial_id>')
-    pass
-
 def test_admin_list_users(self):
     res = self.client().get('/admin/users')
     pass
@@ -69,35 +17,6 @@ def test_admin_unpublish_tutorial(self):
 ###################
 # moderator tests #
 ###################
-
-def test_moderator_get_published_list(self):
-    res = self.client().get('/published')
-    pass
-
-def test_moderator_get_published_tutorial(self):
-    res = self.client().get('/published/<int:tutorial_id>')
-    pass
-
-def test_moderator_get_published_list_by_author(self):
-    res = self.client().get('/published/by-author/<int:author_id>')
-    pass
-
-def test_moderator_get_published_by_tag(self):
-    res = self.client().get('/published/tags/<string:tag>')
-    pass
-
-def test_moderator_get_published_by_tags(self):
-    res = self.client().get('/published/tags/<string:tag1>/<string:tag2>')
-    pass
-
-def test_moderator_submit(self):
-    res = self.client().post('/submit')
-    pass
-
-def test_moderator_edit(self):
-    res = self.client().patch('/edit/<int:tutorial_id>')
-    pass
-
 def test_moderator_get_submitted_list(self):
     res = self.client().get('/submitted')
     pass
@@ -137,27 +56,6 @@ def test_moderator_unpublish_tutorial(self):
 ##################
 # end_user tests #
 ##################
-
-def test_end_user_get_published_list(self):
-    res = self.client().get('/published')
-    pass
-
-def test_end_user_get_published_tutorial(self):
-    res = self.client().get('/published/<int:tutorial_id>')
-    pass
-
-def test_end_user_get_published_list_by_author(self):
-    res = self.client().get('/published/by-author/<int:author_id>')
-    pass
-
-def test_end_user_get_published_by_tag(self):
-    res = self.client().get('/published/tags/<string:tag>')
-    pass
-
-def test_end_user_get_published_by_tags(self):
-    res = self.client().get('/published/tags/<string:tag1>/<string:tag2>')
-    pass
-
 def test_end_user_submit(self):
     res = self.client().post('/submit')
     pass
@@ -181,27 +79,6 @@ def test_end_user_get_unpublished_list(self):
 def test_end_user_get_unpublished_tutorial(self):
     res = self.client().get('/unpublished/<int:tutorial_id>')
     pass
-
-def test_end_user_publish(self):
-    res = self.client().get('/publish/<int:tutorial_id>')
-    pass
-
-def test_end_user_deny(self):
-    res = self.client().patch('/deny/<int:tutorial_id>')
-    pass
-
-def test_end_user_list_users(self):
-    res = self.client().get('/admin/users')
-    pass
-
-def test_end_user_delete_tutorial(self):
-    res = self.client().delete('/admin/tutorial/<int:tutorial_id>')
-    pass
-
-def test_end_user_unpublish_tutorial(self):
-    res = self.client().patch('unpublish_tutorial')
-    pass
-
 ################
 # public tests #
 ################
@@ -225,23 +102,6 @@ def test_public_get_published_by_tag(self):
 def test_public_get_published_by_tags(self):
     res = self.client().get('/published/tags/<string:tag1>/<string:tag2>')
     pass
-
-def test_public_submit(self):
-    res = self.client().post('/submit')
-    pass
-
-def test_public_edit(self):
-    res = self.client().patch('/edit/<int:tutorial_id>')
-    pass
-
-def test_public_get_submitted_list(self):
-    res = self.client().get('/submitted')
-    pass
-
-def test_public_get_submitted_tutorial(self):
-    res = self.client().get('/submitted<int:tutorial_id>')
-    pass
-
 def test_public_get_unpublished_list(self):
     res = self.client().get('/unpublished')
     pass
@@ -260,13 +120,5 @@ def test_public_deny(self):
 
 def test_public_list_users(self):
     res = self.client().get('/admin/users')
-    pass
-
-def test_public_delete_tutorial(self):
-    res = self.client().delete('/admin/tutorial/<int:tutorial_id>')
-    pass
-
-def test_public_unpublish_tutorial(self):
-    res = self.client().patch('unpublish_tutorial')
     pass
 
