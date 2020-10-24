64f64a31c2ce3c884d8d6a93c5cef3427bb8b7b0
bajaco <admin@agyx.org>
2020-10-24 08:42:31 +0000

    Feat: Frontend calling secured endpoints.
    Made a test page to list users with the administrator account
    to ensure that the frontend is requesting and passing JWTs to
    the backend correctly.

--- frontend/flat-tutorials/src/App.js
+++ frontend/flat-tutorials/src/App.js
@@ -1,14 +1,19 @@
-import React, {Component} from 'react';
-import TutorialsList from './components/tutorials-list';
+import React, {useEffect, Component} from 'react';
+//import TutorialsList from './components/tutorials-list';
 import Header from './components/header';
 import Nav from './components/nav.js';
+import Users from './components/users.js';
+import SignupButton from './components/signup-button.js';
+import Profile from './components/profile.js';
+import { Auth0Provider } from '@auth0/auth0-react';
+
 
 class App extends Component {
 
   state = {
-    tutorials: []
+    users: []
   }
-
+  /*
   componentDidMount() {
     fetch('http://localhost:5000/published')
     .then(res => res.json())
@@ -17,13 +22,28 @@ class App extends Component {
     })
     .catch(console.log)
   }
+  */
+
   render() {
     return (
-      <React.Fragment>
-        <Header />
-        <Nav />
-        <TutorialsList tutorials={this.state.tutorials} />
-      </React.Fragment>
+      <Auth0Provider
+        domain='agyx.auth0.com'
+        clientId='btQeJ55ezH0ejA6JSDTTzeTR5NZ4fv5b'
+        responseType='token id_token'
+        redirectUri={window.location.origin}
+        audience='http://localhost:5000/'
+        scope='openid email profile list:users'
+        
+        
+      >
+        <React.Fragment>
+          <Header />
+          <SignupButton />
+          <Nav />
+          <Users />
+          
+        </React.Fragment>
+      </Auth0Provider>
     )
   }
 }
---
+++ frontend/flat-tutorials/src/components/users.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+import { useAuth0 } from '@auth0/auth0-react';
+
+const Users = () => {
+  const { getAccessTokenSilently } = useAuth0();
+  const [ users, setUsers ] = useState()
+  useEffect(() => {
+    (async () => {
+      try {
+        const token = await getAccessTokenSilently({
+          audience: 'http://localhost:5000/',
+          scope: 'list:users',
+        });
+        const response = await fetch('http://localhost:5000/admin/users', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setUsers(await response.json());
+      } catch(e) {
+        console.error(e);
+      }
+    })();
+  }, [getAccessTokenSilently]);
+  
+  if (!users) {
+    return (
+      <div>
+        <h5> No users available </h5>
+      </div>
+    );
+  }
+
+  return (
+    <div> 
+    {users['users'].map((user) => (
+      <h1>{ user.username }</h1>
+    ))}
+    </div>
+  );
+};
+ 
+
+
+export default Users;
