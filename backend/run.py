0c98e06c9994a27898101b34f69a615f93771356
bajaco <admin@agyx.org>
2020-10-29 11:05:30 +0000

    Fix: Procfile and run.py
    Fixed details to ensure smooth deployment on heroku

---
+++ backend/run.py
@@ -0,0 +1,11 @@
+from src import create_app, db
+from dotenv import load_dotenv
+load_dotenv('.flaskenv')
+app = create_app()
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run()
+
+
+
