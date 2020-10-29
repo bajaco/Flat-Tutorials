997a2b9742d4663734c36c877dc301dc19efdb7f
bajaco <admin@agyx.org>
2020-10-29 14:53:00 +0000

    Fix: Environment Variables Environment variables switched to auth.py

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -10,10 +10,7 @@ db = SQLAlchemy()
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    CORS(app)
-    AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
-    ALGORITHMS = os.environ.get('ALGORITHMS')
-    API_AUDIENCE = os.environ.get('API_AUDIENCE')
+    CORS(app) 
     DATABASE_NAME = os.environ.get('DATABASE_NAME')
     DATABASE_USER = os.environ.get('DATABASE_USER')
     DATABASE_LOCATION = os.environ.get('DATABASE_LOCATION')
--- backend/src/auth.py
+++ backend/src/auth.py
@@ -4,6 +4,11 @@ from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
 from werkzeug.exceptions import abort
+import os
+
+AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
+ALGORITHMS = os.environ.get('ALGORITHMS')
+API_AUDIENCE = os.environ.get('API_AUDIENCE')
 
 # AuthError Exception
 '''
